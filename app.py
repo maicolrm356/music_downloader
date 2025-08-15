120bbc6419b3425c906d43d801c3c444ec003cf2
auxanalista <auxanalista@consuerte.com.co>
2025-08-15 16:57:14 -0500

    Prueba de nombres con el album de panda

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import pandas as pd
 
 import yt_dlp as yt
 
-URLS = ['https://www.youtube.com/watch?v=o1tj2zJ2Wvg&list=OLAK5uy_n13hmdsIozcCRyaY4cRDuuviphpfbzPrw']
+# prueba con los amantes sunt amentes
+URLS = ['https://music.youtube.com/playlist?list=OLAK5uy_nhExNbsuT4shEKEVHqc_8VCjpfjJFT_lc']
 # URLS = ['https://www.youtube.com/watch?v=oabjND9QW8Q&list=RD4QhFuJWqTAc&index=2']
+
 path = 'C:/Users/maico/Music/music/Guns N Roses'
 ydl_opts = {
     'ffmpeg_location': r'C:\Users\maico\Downloads\ffmpeg-2025-08-07-git-fa458c7243-essentials_build\ffmpeg-2025-08-07-git-fa458c7243-essentials_build\bin',
     'format': 'm4a/bestaudio/best',
-    'outtmpl': f'{path}/%(title)s.%(ext)s',
-    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+    'outtmpl': f'{path}/%(track)s.%(ext)s',
+    # ℹ︝ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
     'postprocessors': [
         {  # Extract audio using ffmpeg
             'key': 'FFmpegExtractAudio',
