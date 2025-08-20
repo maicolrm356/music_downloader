07d972c839bfa0109a5e122695722e265ac42b8d
Michael <maicolrm356@gmail.com>
2025-08-19 19:56:18 -0500

    mejora de intentos de descarga

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import yt_dlp as yt
 
 # prueba con los amantes sunt amentes
 URLS = ['https://music.youtube.com/playlist?list=OLAK5uy_nhExNbsuT4shEKEVHqc_8VCjpfjJFT_lc']
+
+#APETITO DE DESTRUCCION
+URLS = ['https://music.youtube.com/playlist?list=OLAK5uy_ltQiuNnoBifeiEYSjm5Ac1Z-x3ydiYrq4'] 
 # URLS = ['https://www.youtube.com/watch?v=oabjND9QW8Q&list=RD4QhFuJWqTAc&index=2']
 
-path = 'C:/Users/maico/Music/music/Guns N Roses'
+path = 'C:/Users/maico/Music/music/PXNDX'
 ydl_opts = {
     'ffmpeg_location': r'C:\Users\maico\Downloads\ffmpeg-2025-08-07-git-fa458c7243-essentials_build\ffmpeg-2025-08-07-git-fa458c7243-essentials_build\bin',
     'format': 'm4a/bestaudio/best',
@@ -20,7 +23,11 @@ ydl_opts = {
         {
             'key': 'FFmpegMetadata',
         }
-    ]                                
+    ],
+    'retries': 10,             # más intentos
+    'fragment_retries': 10,    # más intentos por fragmento
+    'socket_timeout': 30,      # segundos de espera (por defecto es 10)
+    'http_chunk_size': 10485760,  # fuerza descargas en bloques de 10 MB                           
 }
 
 with yt.YoutubeDL(ydl_opts) as ydl:
